242a06114c39ea198050577f04f015baf8976e05
robjrdev <85550412+robjrdev@users.noreply.github.com>
2022-10-27 08:06:12 +0800

    Remove inner-nav

--- banking-and-budget-CRA/src/components/sidepanels/index.js
+++ banking-and-budget-CRA/src/components/sidepanels/index.js
@@ -1,4 +1,4 @@
-const SidePanel = ({transactionButton}) => {
+const SidePanel = ({ transactionButton }) => {
   const handleClick = () => {
     const sidePanel = document.querySelector(".nav-bar");
     const innerSidePanel = document.querySelector(".inner-nav");
@@ -15,32 +15,16 @@ const SidePanel = ({transactionButton}) => {
       <button className="side-panel-btn__toggle" onClick={handleClick}>
         Toggle
       </button>
-      {/* Inner nav  */}
-      <div className="inner-nav hide">
-        <ul className="inner-nav-list">
-          <li className="inner-nav-items">
-            <button className="btn btn-inner-nav">Home</button>
-          </li>
-          <li className="inner-nav-items">
-            <button className="btn btn-inner-nav">Withdraw</button>
-          </li>
-          <li className="inner-nav-items">
-            <button className="btn btn-inner-nav">Deposit</button>
-          </li>
-          <li className="inner-nav-items">
-            <button className="btn btn-inner-nav">Savings</button>
-          </li>
-        </ul>
-      </div>
-
       {/* NAVBAR */}
       <nav className="nav-bar flex-column">
         <ul className="nav-list flex-column">
           <li className="nav-items">
-            <button className="btn btn-nav-links" >Home</button>
+            <button className="btn btn-nav-links">Home</button>
           </li>
           <li className="nav-items">
-            <button className="btn btn-nav-links" onClick={transactionButton}>Transactions</button>
+            <button className="btn btn-nav-links" onClick={transactionButton}>
+              Transactions
+            </button>
           </li>
           <li className="nav-items">
             <button className="btn btn-nav-links">Accounts</button>
