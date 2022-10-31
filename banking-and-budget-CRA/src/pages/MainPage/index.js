a5de0fa89a55dce1f33dfd70f093e87841f6cbae
Henri Nicholli Muñoz <hnbmunoz@gmail.com>
2022-10-31 08:49:06 +0800

    Partially-Done-with-design

--- banking-and-budget-CRA/src/pages/MainPage/index.js
+++ banking-and-budget-CRA/src/pages/MainPage/index.js
@@ -1,17 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { LoginPage } from "../LoginPage";
 import { PanelSections, PanelSectionHolder } from "../../components/panels";
 import { NavigationContainer, NavigationItems } from "../../components/navigation";
+import * as NavIcons from "../../components/navigation/navIcons";
 import Header from "../../components/header";
 
 import Dashboard from "../Dashboard";
 import Deposit from "../Deposit";
 import Withdraw from "../Withdraw";
 import FundTransfer from "../FundTransfer";
+import Accounts from "../Account";
+import Messages from "../Messages";
+import SwitchAccount from "../SwitchAccount";
+import Transactions from "../Transactions";
 
 export const MainPage = () => {
   const [verifiedAccount, setVerifiedAccount] = useState(false);
   const [displayIndex, setDisplayIndex] = useState(0);
+  const [openNav, setOpenNav] = useState(true);
+
+  const handleToggleNavbar = (e) => {
+    setOpenNav(!openNav)
+  }
 
   const handleUserLogin = () => {
     setVerifiedAccount(true);
@@ -26,15 +36,73 @@ export const MainPage = () => {
     const panelIndex = btnClicked.dataset.id;
     setDisplayIndex(panelIndex);
   };
+  
+  useEffect(() => {
+    document.querySelector('.nav-widget').classList.toggle('close-widget')
+    return () => {}
+  }, [openNav])
+  
+
+
 
   return (
     <div>
-       <NavigationContainer showNav={verifiedAccount}>
-          <NavigationItems itemName="Dashboard" panelIdx={0} itemClick={handleSwitchPanel}/>
-          <NavigationItems itemName="Deposit" panelIdx={1} itemClick={handleSwitchPanel}/>
-          <NavigationItems itemName="Withdraw" panelIdx={2} itemClick={handleSwitchPanel}/>
-          <NavigationItems itemName="Fund Transfer" panelIdx={3} itemClick={handleSwitchPanel}/>
-          <NavigationItems itemName="Log Out" itemClick={handleUserLogout}/>
+       <NavigationContainer showNav={verifiedAccount} navToggle={handleToggleNavbar}>
+          <NavigationItems
+            itemName="Accounts"
+            panelIdx={0}
+            itemClick={handleSwitchPanel}
+            navIcon={<NavIcons.AccountIcon/>}
+            navOpen={openNav}
+          />
+          <NavigationItems
+            itemName="Transactions"
+            panelIdx={1}
+            itemClick={handleSwitchPanel}
+            navIcon={<NavIcons.TransactionIcon />}
+            navOpen={openNav}
+          />
+          <NavigationItems
+            itemName="Deposit"
+            panelIdx={2}
+            itemClick={handleSwitchPanel}
+            navIcon={<NavIcons.DepositIcon />}
+            navOpen={openNav}
+          />
+          <NavigationItems
+            itemName="Withdraw"
+            panelIdx={3}
+            itemClick={handleSwitchPanel}
+            navIcon={<NavIcons.WithdrawIcons />}
+            navOpen={openNav}
+          />
+          <NavigationItems
+            itemName="Transfer"
+            panelIdx={4}
+            itemClick={handleSwitchPanel}
+            navIcon={<NavIcons.FundTransferIcon />}
+            navOpen={openNav}
+          />
+          <NavigationItems
+            itemName="Messages"
+            panelIdx={5}
+            itemClick={handleSwitchPanel}
+            navIcon={<NavIcons.MessagesIcon />}
+            navOpen={openNav}
+          />
+          <NavigationItems
+            itemName="SwitchAccount"
+            panelIdx={6}
+            itemClick={handleSwitchPanel}
+            navIcon={<NavIcons.SwitchAccountIcon />}
+            navOpen={openNav}
+            />            
+          <NavigationItems
+            itemName="Log Out"
+            itemClick={handleUserLogout}
+            navIcon={<NavIcons.LogOutIcons />}
+            navOpen={openNav}
+          />
         </NavigationContainer> 
 
       {verifiedAccount ? (
@@ -52,7 +120,10 @@ export const UserInterface = ({displayPanel}) => {
     <Header />
     <PanelSectionHolder panelIdx={displayPanel}>      
       <PanelSections>
-        <Dashboard />
+        <Accounts />
+      </PanelSections>
+      <PanelSections>
+        <Transactions />
       </PanelSections>
       <PanelSections>
         <Deposit />
@@ -63,7 +134,13 @@ export const UserInterface = ({displayPanel}) => {
       <PanelSections>
         <FundTransfer />
       </PanelSections>
-    </PanelSectionHolder>
+      <PanelSections>
+        <Messages />
+      </PanelSections>
+      <PanelSections>
+        <SwitchAccount />
+      </PanelSections>
+    </PanelSectionHolder> 
   </div>
     
   );
---
+++ banking-and-budget-CRA/src/pages/SwitchAccount/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SwitchAccount = () => {
+  return (
+    <div>Switch Account</div>
+  )
+}
+
+export default SwitchAccount
