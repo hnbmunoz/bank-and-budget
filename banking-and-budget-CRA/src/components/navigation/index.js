80a023c873ca86d86355c7573bc4c5b0087718e7
Henri Nicholli Muñoz <hnbmunoz@gmail.com>
2022-11-02 14:09:41 +0800

    Additional Interface Designs Implemented

--- banking-and-budget-CRA/src/components/navigation/index.js
+++ banking-and-budget-CRA/src/components/navigation/index.js
@@ -1,23 +1,28 @@
-import React,{ useRef, useState } from 'react'
+import React,{ useRef, useEffect } from 'react'
 import useDraggable from '../../utilities/hooks/useDraggable'
 import { ToggleButton } from '../button';
 import { NavToggler } from './navIcons';
 
-export const NavigationContainer = ({children, showNav = false, navToggle}) => {
+export const NavigationContainer = ({children, showNav = false, navToggle, selectedPanel=0}) => {
   return (
     <div  className='nav-container flex-column' style={{ display: !showNav && "none" }}>     
-      <NavigationWidget isOpen={navToggle}>
+      <NavigationWidget isOpen={navToggle} selectedPanel={selectedPanel}>
         {children}
       </NavigationWidget>
     </div>
   )
 }
 
-export const NavigationWidget = ({children, isOpen}) => {
+export const NavigationWidget = ({children, isOpen, selectedPanel=0}) => {
+  useEffect(() => {    
+    let targetEl = document.querySelectorAll(".nav-items");
+    targetEl.forEach((item, idx) => {
+      selectedPanel == idx
+        ? item.classList.add("active-items")
+        : item.classList.remove("active-items");
+    });
+  }, [selectedPanel, isOpen]);
   // const [openNav, setOpenNav] = useState(true);
-  // const handleToggleNavbar = (e) => {
-  //   // alert('test')
-  // }
   // const dragRef = useRef(null);
   // useDraggable(dragRef);
   return (
@@ -34,11 +39,8 @@ export const NavigationWidget = ({children, isOpen}) => {
 }
 
 export const NavigationItems = ({itemName, panelIdx = 0, itemClick, navIcon, navOpen=true}) => {
-  function testClick(e) {
-    debugger
-  }
   return (
-    <div className={`nav-items flex-row ${!navOpen && 'close-item'}`} onClick={itemClick} data-id={panelIdx} >
+    <div className={`nav-items flex-row ${!navOpen && 'close-item'}`} onClick={itemClick} data-id={panelIdx}>
       {navIcon}
       {navOpen && itemName}
     </div>
