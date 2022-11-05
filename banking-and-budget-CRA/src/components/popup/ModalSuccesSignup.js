f6a26b501d40037e542ef59cb05e4f48642362f8
Ruther Dio <rutherdio.x04@gmail.com>
2022-11-05 11:49:31 +0800

    add loading and popupmodal on signup

---
+++ banking-and-budget-CRA/src/components/popup/ModalSuccesSignup.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ModalSuccesSignup = () => {
+  return (
+    <div className="modalBackground">
+      <div className="modalBackgroundCon">
+        <div className="modalContainer">
+          <div className="title">
+            <h1>Sign Up Complete!</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ModalSuccesSignup
