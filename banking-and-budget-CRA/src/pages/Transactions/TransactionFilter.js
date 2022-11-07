3df650ea63a4369a81b313b9d635eddbf46cdbc7
robjrdev <85550412+robjrdev@users.noreply.github.com>
2022-11-07 15:31:09 +0800

    Add trasaction filter and design

--- banking-and-budget-CRA/src/pages/Transactions/TransactionFilter.js
+++ banking-and-budget-CRA/src/pages/Transactions/TransactionFilter.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import { GlowingButton } from "../../components/button";
-const TransactionFilter = ({onTransaction, onDate}) => {
+const TransactionFilter = ({onTransaction, onDate, currentDate}) => {
   const changeTransactionHandler = (e)=>{
    onTransaction(e.target.value)
-  }
-
+  };
   const changeDateHandler = (e)=>{
    onDate(e.target.value); 
-  }
+  };
   return (
     <div className="transaction-filter">
       <div className="transaction-filter__control">
-
         <div className="transaction-filter__date">
             <h3>Select Date</h3>  
-            <input type="date" onChange={changeDateHandler}/>
+            <input type="date" onChange={changeDateHandler} value={currentDate}/>
         </div>       
 
         <div className="transaction-filter__transactions">
-            <h3>Filter by Transaction</h3>  
-            <GlowingButton value="All Transactions" buttonClick={changeTransactionHandler} displayText="All Transactions" width='5px'/> 
-            <GlowingButton value="Deposit"  displayText='Deposit' buttonClick={changeTransactionHandler}/>
-            <GlowingButton value="Withdraw" displayText='Withdraw' buttonClick={changeTransactionHandler}/>
-            <GlowingButton value="Transfer" displayText='Transfer' buttonClick=
-            {changeTransactionHandler}/>   
+            <div className="transaction-btn">
+              <GlowingButton value="All Transactions" buttonClick={changeTransactionHandler} displayText="All Transactions"/> 
+              <GlowingButton value="Deposit"  displayText='Deposit' buttonClick={changeTransactionHandler}/>
+              <GlowingButton value="Withdraw" displayText='Withdraw' buttonClick={changeTransactionHandler}/>
+              <GlowingButton value="Transfer" displayText='Transfer' buttonClick=
+              {changeTransactionHandler}/>   
+            </div>
         </div>
       </div>
     </div>
--- banking-and-budget-CRA/src/pages/Transactions/TransactionItem.js
+++ banking-and-budget-CRA/src/pages/Transactions/TransactionItem.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const TransactionItem = ({title,amount,description,destination}) => {
+const TransactionItem = ({title, amount, description, date}) => {
+  const newAmount = Math.sign(amount) === -1 ? amount * -1 : amount;
+  // console.log(date);
+ 
   return (
-        <div>    
-          <h3>{title}</h3>
-          <p>{amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</p>
-          <p>{description}</p>
+        <div className="transaction-item">
+          <div className="transaction-item__date">{date}</div>
+          <div className="transaction-item__info"> 
+              {/* For discussion */}
+              <h4 className='transaction-item__title'>{title}</h4>  
+              <h4 className='transaction-item__description'>{description}</h4>
+              <div className='transaction-item__amount'>
+                PHP {newAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+              </div>              
+          </div>
+         
         </div>
   )
 };
