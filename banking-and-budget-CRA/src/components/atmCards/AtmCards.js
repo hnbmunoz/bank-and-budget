b66460413d8296ddfd087a73db65bc60e87779df
robjrdev <85550412+robjrdev@users.noreply.github.com>
2022-11-18 17:56:39 +0800

    add user info to card

--- banking-and-budget-CRA/src/components/atmCards/AtmCards.js
+++ banking-and-budget-CRA/src/components/atmCards/AtmCards.js
@@ -1,12 +1,16 @@
 import React from 'react'
-
+import { useState, useEffect} from "react";
+import { GetAccountBalance} from "../../utilities/utilities";
 const AtmCards = () => {
   return (
     <></>
   )
 }
 
-export const GalaxyAtm = ({logo, backgroundImage, cardType}) => {
+export const GalaxyAtm = ({userBalance, userName, userData, logo, backgroundImage, cardType}) => {
+
+  
+  
     return (
         <div className="container cards">
           <div className="payment space">
@@ -25,50 +29,19 @@ export const GalaxyAtm = ({logo, backgroundImage, cardType}) => {
               </div>
               <fieldset className="card-number card-number__fields">
                 <legend className="card-number__title">CARD NUMBER</legend>
-                  <input type="tel" className="card-number__part" maxLength={4} />
-                  <input type="tel" className="card-number__part" maxLength={4} />
-                  <input type="tel" className="card-number__part" maxLength={4} />
-                  <input type="tel" className="card-number__part" maxLength={4} />
-              </fieldset>
+                <div className="flex-row" style={{justifyContent: 'space-between', width: '100%'}}>
+                  <div className="card-number__part">{userData.accountNumber}</div>
+                  <div className="user-balance">{userBalance}</div>
+                </div>                    
+              </fieldset>         
               <div className="holder-valid-container">
                 <label className="card-holder">
                   <span className="card-holder__title">CARD HOLDER</span>
-                  <input name="ccname" autoComplete="cc-name" />
-                </label>
+                  <div>{userName}</div>
+                </label>          
                 <fieldset className="valid-thru">
                   <legend className="valid-trhu__title">VALID THRU</legend>
-                  <label>
-                    <span className="sr-only">Month</span>
-                    <select className="valid-thru__part month" id="valid-thru_part1">
-                      <optgroup label="Month">
-                        <option value=""></option>
-                        <option value="01">01</option>
-                        <option value="02">02</option>
-                        <option value="03">03</option>
-                        <option value="04">04</option>
-                        <option value="05">05</option>
-                        <option value="06">06</option>
-                        <option value="07">07</option>
-                        <option value="08">08</option>
-                        <option value="09">09</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                      </optgroup>
-                    </select>
-                  </label>
-                  <label>
-                    <span className="sr-only">Year</span>
-                    <select className="valid-thru__part year" id="valid-thru_part2">
-                      <optgroup label="Year">
-                        <option value=""></option>
-                        <option value="2022">22</option>
-                        <option value="2023">23</option>
-                        <option value="2024">24</option>
-                        <option value="2025">25</option>
-                      </optgroup>
-                    </select>
-                  </label>
+                  <div>{userData.accountExpiry}</div>
                 </fieldset>
               </div>
             </div>
@@ -84,7 +57,7 @@ export const GalaxyAtm = ({logo, backgroundImage, cardType}) => {
   }
 
 
-export const RedAtm = ({logo, backgroundImage, cardType}) => {
+export const RedAtm = ({userBalance, userName, userData,logo, backgroundImage, cardType}) => {
     return (
         <div className="container cards">
           <div className="payment sparkasse">
@@ -103,50 +76,19 @@ export const RedAtm = ({logo, backgroundImage, cardType}) => {
               </div>
               <fieldset className="card-number card-number__fields">
                 <legend className="card-number__title">CARD NUMBER</legend>
-                  <input type="tel" className="card-number__part" maxLength={4} />
-                  <input type="tel" className="card-number__part" maxLength={4} />
-                  <input type="tel" className="card-number__part" maxLength={4} />
-                  <input type="tel" className="card-number__part" maxLength={4} />
+                <div className="flex-row" style={{justifyContent: 'space-between', width: '100%'}}>
+                  <div className="card-number__part">{userData.accountNumber}</div>
+                  <div className="user-balance">{userBalance}</div>
+                </div>             
               </fieldset>
               <div className="holder-valid-container">
                 <label className="card-holder">
                   <span className="card-holder__title">CARD HOLDER</span>
-                  <input name="ccname" autoComplete="cc-name" />
-                </label>
+                  <div>{userName}</div>
+                </label>          
                 <fieldset className="valid-thru">
                   <legend className="valid-trhu__title">VALID THRU</legend>
-                  <label>
-                    <span className="sr-only">Month</span>
-                    <select className="valid-thru__part month" id="valid-thru_part1">
-                      <optgroup label="Month">
-                        <option value=""></option>
-                        <option value="01">01</option>
-                        <option value="02">02</option>
-                        <option value="03">03</option>
-                        <option value="04">04</option>
-                        <option value="05">05</option>
-                        <option value="06">06</option>
-                        <option value="07">07</option>
-                        <option value="08">08</option>
-                        <option value="09">09</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                      </optgroup>
-                    </select>
-                  </label>
-                  <label>
-                    <span className="sr-only">Year</span>
-                    <select className="valid-thru__part year" id="valid-thru_part2">
-                      <optgroup label="Year">
-                        <option value=""></option>
-                        <option value="2022">22</option>
-                        <option value="2023">23</option>
-                        <option value="2024">24</option>
-                        <option value="2025">25</option>
-                      </optgroup>
-                    </select>
-                  </label>
+                  <div>{userData.accountExpiry}</div>
                 </fieldset>
               </div>
             </div>
@@ -161,7 +103,7 @@ export const RedAtm = ({logo, backgroundImage, cardType}) => {
     )
   }
 
-export const PinkAtm = ({ logo, backgroundImage, cardType }) => {
+export const PinkAtm = ({userBalance, userName, userData, logo, backgroundImage, cardType }) => {
     return (
       <div className="container cards">
         <div className="payment klarna">
@@ -177,55 +119,22 @@ export const PinkAtm = ({ logo, backgroundImage, cardType }) => {
               </div>
             </div>
             <fieldset className="card-number card-number__fields">
-              <legend className="card-number__title">CARD NUMBER</legend>
-              <input type="tel" className="card-number__part" maxLength={4} />
-              <input type="tel" className="card-number__part" maxLength={4} />
-              <input type="tel" className="card-number__part" maxLength={4} />
-              <input type="tel" className="card-number__part" maxLength={4} />
-            </fieldset>
-            <div className="holder-valid-container">
-              <label className="card-holder">
-                <span className="card-holder__title">CARD HOLDER</span>
-                <input name="ccname" autoComplete="cc-name" />
-              </label>
-              <fieldset className="valid-thru">
-                <legend className="valid-trhu__title">VALID THRU</legend>
-                <label>
-                  <span className="sr-only">Month</span>
-                  <select
-                    className="valid-thru__part month"
-                    id="valid-thru_part1">
-                    <optgroup label="Month">
-                      <option value=""></option>
-                      <option value="01">01</option>
-                      <option value="02">02</option>
-                      <option value="03">03</option>
-                      <option value="04">04</option>
-                      <option value="05">05</option>
-                      <option value="06">06</option>
-                      <option value="07">07</option>
-                      <option value="08">08</option>
-                      <option value="09">09</option>
-                      <option value="10">10</option>
-                      <option value="11">11</option>
-                      <option value="12">12</option>
-                    </optgroup>
-                  </select>
-                </label>
-                <label>
-                  <span className="sr-only">Year</span>
-                  <select className="valid-thru__part year" id="valid-thru_part2">
-                    <optgroup label="Year">
-                      <option value=""></option>
-                      <option value="2022">22</option>
-                      <option value="2023">23</option>
-                      <option value="2024">24</option>
-                      <option value="2025">25</option>
-                    </optgroup>
-                  </select>
-                </label>
+                <legend className="card-number__title">CARD NUMBER</legend>
+                <div className="flex-row" style={{justifyContent: 'space-between', width: '100%'}}>
+                  <div className="card-number__part">{userData.accountNumber}</div>
+                  <div className="user-balance">{userBalance}</div>
+                </div>             
               </fieldset>
-            </div>
+              <div className="holder-valid-container">
+                <label className="card-holder">
+                  <span className="card-holder__title">CARD HOLDER</span>
+                  <div>{userName}</div>
+                </label>          
+                <fieldset className="valid-thru">
+                  <legend className="valid-trhu__title">VALID THRU</legend>
+                  <div>{userData.accountExpiry}</div>
+                </fieldset>
+              </div>
           </div>
           <div className="back-card">
             <label className="cvc">
--- banking-and-budget-CRA/src/pages/SwitchAccount/index.js
+++ banking-and-budget-CRA/src/pages/SwitchAccount/index.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import Modal from "../../components/modal";
 import Slider from "../../components/slider/Slider";
 import { Shifting } from "../../components/carousel";
+import useLocalStorageStore from "../../utilities/hooks/useLocalStorage";
+import { GetAccountBalance, GetTransactionBalance } from "../../utilities/utilities";
+
 
 //Atm Cards
 import { GalaxyAtm, PinkAtm, RedAtm } from "../../components/atmCards/AtmCards";
@@ -20,7 +23,78 @@ import VisaWhiteLogo from "../../assets/wallpapers/VisaWhiteLogo";
 import VisaBlackLogo from "../../assets/wallpapers/VisaBlackLogo";
 import MasterCardLogo from "../../assets/wallpapers/MasterCardLogo";
 
-const SwitchAccount = () => {  
+const SwitchAccount = ({getUserCode, displayPanel}) => {
+  const [userStore, setUserStore, getUserStore] = useLocalStorageStore(
+    "registeredUsers",
+    []
+  );
+  const [userAccount, setUserAccount, getUserAccount] = useLocalStorageStore('userAccounts',[])
+  const [userTransactions, setUserTransaction, getUserTransactions] = useLocalStorageStore("userTransaction", []);
+  const [userName, setUserName] = useState('');
+  const [galaxyDetail, setGalaxyDetail] = useState('');
+  const [klarnaDetail, setKlarnaDetail] = useState('');
+  const [sparksseDetail, setSparksseDetail] = useState('');
+  const [galaxyBalance, setGalaxyBalance] = useState(0)
+  const [klarnaBalance, setKlarnaBalance] = useState(0)
+  const [sparksseBalance, setSparksseBalance] = useState(0)
+
+
+  useEffect(() => {
+    getUserTransactions();
+    getUserAccount();
+    getUserStore();
+    getAccountBalance()
+    return () => {};
+  }, [displayPanel]);
+
+  const getAccountBalance = (acctNumber) => {       
+    const galaxyATM = userTransactions.filter(
+      (user) => user.userCode === getUserCode && user.accountNumber === acctNumber.galaxy
+    );
+    const klarnaATM = userTransactions.filter(
+      (user) => user.userCode === getUserCode && user.accountNumber === acctNumber.klarna
+    );
+    const sparkssekATM = userTransactions.filter(
+      (user) => user.userCode === getUserCode && user.accountNumber === acctNumber.sparksse
+    );
+    const galaxyTotalBalance = GetAccountBalance(galaxyATM);
+    const klarnaTotalBalance = GetAccountBalance(klarnaATM);
+    const sparksseTotalBalance = GetAccountBalance(sparkssekATM);
+
+    setGalaxyBalance(galaxyTotalBalance);
+    setKlarnaBalance(klarnaTotalBalance);
+    setSparksseBalance(sparksseTotalBalance);
+  }
+
+  useEffect(() => {
+    userAccount.length > 0 && getDetails();
+    return () => {};
+  },[displayPanel, userAccount]);
+
+  const getDetails = () => {
+    const userData = userAccount.filter(
+      (user) => user.accountUser === `${getUserCode}`)
+
+  const userName = userStore.filter(
+      (user) => user.userCode === `${getUserCode}`).map((user) =>
+      {setUserName(user.userFullName)})
+
+  const galaxyATM = userData.find((data) => data.accountType === 'Galaxy');
+  const klarnaATM = userData.find((data) => data.accountType === 'Klarna');
+  const sparkssekATM = userData.find((data) => data.accountType === 'Sparksse');
+
+    getAccountBalance({galaxy: galaxyATM.accountNumber,klarna: klarnaATM.accountNumber, sparksse: sparkssekATM.accountNumber,})
+
+    setGalaxyDetail(galaxyATM)
+    setKlarnaDetail(klarnaATM)
+    setSparksseDetail(sparkssekATM)
+  
+  };
+
+
+
+
+
   return (
     <Shifting>
       <div data-carousel="galaxy" className="carousel-div">
@@ -28,6 +102,9 @@ const SwitchAccount = () => {
             backgroundImage={Galaxy}
             logo={<LogoN26 />}
             cardType={<VisaWhiteLogo />}
+            userData={galaxyDetail}
+            userName={userName}
+            userBalance={galaxyBalance}
           />
       </div> 
       <div data-carousel="klarna" className="carousel-div">
@@ -35,6 +112,9 @@ const SwitchAccount = () => {
         backgroundImage={<KlarnaBackgroundImg />}
         logo={<LogoKlarna />}
         cardType={<VisaBlackLogo />}
+        userData={klarnaDetail}
+        userName={userName}
+        userBalance={klarnaBalance}
       />
       </div>
       <div data-carousel="sparksse" className="carousel-div">
@@ -42,6 +122,9 @@ const SwitchAccount = () => {
         backgroundImage={""}
         logo={<LogoSparksse />}
         cardType={<MasterCardLogo />}
+        userData={sparksseDetail}
+        userName={userName}
+        userBalance={sparksseBalance}
       />
       </div>
     </Shifting>
